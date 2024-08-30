a7ed6611e4bdb28d99fa209120942663225489f3
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-08-31 02:52:55 +0300

    Enabled CORS, made validation rules stricter

--- backend/src/url-shortener/dto/short-url-create.dto.ts
+++ backend/src/url-shortener/dto/short-url-create.dto.ts
@@ -1,6 +1,9 @@
 import { IsUrl } from 'class-validator';
 
 export class ShortUrlCreateDto {
-  @IsUrl()
+  @IsUrl({
+    protocols: ['http', 'https'],
+    require_protocol: true,
+  })
   url: string;
 }
