9cf04738683b6b7d0e1eb554521ec8274ba1ae7d
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-08-31 11:52:27 +0300

    What if no CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
-  app.enableCors({
-    origin: configService.get('FRONTEND_URL'),
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin'],
-  });
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
