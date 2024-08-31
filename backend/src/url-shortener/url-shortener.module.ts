fcf7678c87d7036a732f925d4456e7c8456c8ab7
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-08-31 03:06:28 +0300

    Try fixing drizzle module import

--- backend/src/url-shortener/url-shortener.module.ts
+++ backend/src/url-shortener/url-shortener.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { URLShortenerController } from './url-shortener.controller';
 import { URLShortenerService } from './url-shortener.service';
-import { DrizzleModule } from 'src/drizzle/drizzle.module';
 import { ShortCodeGeneratorService } from './short-code-generator.service';
 import { URLVisitListener } from './listeners/url-visit.listener';
+import { DrizzleModule } from '../drizzle/drizzle.module';
 
 @Module({
   imports: [DrizzleModule],
