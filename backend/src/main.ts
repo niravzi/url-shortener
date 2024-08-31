e61540f5cc1d1c097e836fad773866595296a7c7
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-08-31 03:35:25 +0300

    CORS again

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.enableCors({
-    origin: true,
+    origin: configService.get('FRONTEND_URL'),
     methods: ['GET', 'POST', 'OPTIONS'],
   });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
